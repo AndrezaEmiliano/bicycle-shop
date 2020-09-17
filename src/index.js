6ce415eb5f5586e57dfc73a79a4a644b9d9b06e5
AndrezaEmiliano <andreza.emiliano@exactaworks.com.br>
2020-09-17 16:49:56 -0300

    refactor: PR adjustments

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
-var products = document.querySelector(".product-list");
+var productsElement = document.querySelector(".product-list");
 
-var list = "";
+var cardProductList = "";
 productList.forEach(function (product) {
-  list += `<li class="card">
-            <img
-              class="card__image"
-              src = ${product.image} 
-              alt= ${product.alt}
-            />
+  cardProductList += `<li class="card">
+                        <img
+                          class="card__image"
+                          src = ${product.image} 
+                          alt= Imagem do produto ${product.description}
+                        />
 
-            <div class="card__info">
-              <p class="card__text">
-                ${product.description}
-                <span class="display--block font--size-1">R$ ${product.price
-                  .toFixed(2)
-                  .toString()
-                  .replace(".", ",")}</span>
-              </p>
+                        <div class="card__info">
+                          <p class="card__text">
+                            ${product.description}
+                            <span class="display--block font--size-1">R$ ${product.price
+                              .toFixed(2)
+                              .toString()
+                              .replace(".", ",")}</span>
+                          </p>
 
-              <button class="button">Adicionar ao carrinho</button>
-            </div>
-          </li>`;
+                          <button class="button">Adicionar ao carrinho</button>
+                        </div>
+                      </li>`;
 });
 
-products.innerHTML = list;
+productsElement.innerHTML = cardProductList;
