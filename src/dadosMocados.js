a8011eb34924f7ad2b8b2ba1830d4aafed7b6304
AndrezaEmiliano <andreza.emiliano@exactaworks.com.br>
2020-09-17 18:09:58 -0300

    refine-PR: variable adjustment and skipping lines

--- src/dadosMocados.js
+++ src/dadosMocados.js
@@ -5,35 +5,30 @@ const productList = [
     category: "bicycle",
     price: 2250.0,
   },
-
   {
     image: "/assets/images/products/speed-bike.png",
     description: "Speed Bike Emigrantes",
     category: "bicycle",
     price: 5640.0,
   },
-
   {
     image: "/assets/images/products/classic-bike.png",
     description: "Chinatown Classic Bike",
     category: "bicycle",
     price: 799.0,
   },
-
   {
     image: "/assets/images/products/gloves.png",
     description: "Luvas Tyson",
     category: "gloves",
     price: 189.0,
   },
-
   {
     image: "/assets/images/products/squeezer.png",
     description: "Squeezer Squirtle",
     category: "bottle",
     price: 30.0,
   },
-
   {
     image: "/assets/images/products/helmet.png",
     description: "Capacete Dragon Born",
